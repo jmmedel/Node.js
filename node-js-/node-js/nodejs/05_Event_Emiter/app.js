cae58e80bd858e5d3852642601a041312456fb2a
john <jm.kagaya@gmail.com>
2018-06-06 09:45:23 -0700

    done

--- node-js-/node-js/nodejs/05_Event_Emiter/app.js
+++ node-js-/node-js/nodejs/05_Event_Emiter/app.js
@@ -1,9 +1,24 @@
 
-var stuff = require("./stuff");
+var event = require('events');
+var util = require('util');
 
-console.log(stuff.adder(1,2));
-console.log(stuff.counter(["john","crytal","jane"]));
-console.log(stuff.adder(stuff.pi,5));
+var Person = function(name){
+    this.name = name;
+
+}
 
 
+util.inherits(Person, events.EventEmitter);
 
+var james = new Person('james');
+var mary = new Person('Mary');
+var ryu = new Person('ryu');
+var people = [james,mary,ryu];
+people.forEach(function(Person){
+    person.on('speak',function(msg){
+        console.log(person.name + 'Said: ' + msg);
+    });
+});
+
+
+james.emit('speak','hey dudes');
