192766879a4a77e47a78231b72aecc165b04f1bb
john <jm.kagaya@gmail.com>
2018-06-06 08:23:52 -0700

    Done

--- node-js-/node-js/Practice/01_node.js
+++ node-js-/node-js/Practice/01_node.js
@@ -1,6 +1,17 @@
 console.log("Hello Saif, You are learning Node.js!");
+console.log("Hello world this is nodejs")
 
 setTimeout(function(){
     console.log("Three seconds \n have passed!");
 
 },3000);
+
+
+setTimeout
+
+setTimeout(function(){
+    console.log("3seconds");
+
+},3000);
+
+setTimeout
